10cb5db6e2dc98b93ad24ddbe5ffb6ceeb9b9e2a
lucaszdev <lucas.lima.tlou@icloud.com>
2022-12-26 11:17:33 -0300

    Initial Commit

---
+++ MyExpenses/MyExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyExpensesApp.swift
+//  MyExpenses
+//
+//  Created by Premiersoft on 26/12/22.
+//
+
+import SwiftUI
+
+@main
+struct MyExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
